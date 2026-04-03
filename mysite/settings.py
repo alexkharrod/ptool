82fd9d182b8be22b6185b4cda7edceba5f274513
Alex <alexkharrod@gmail.com>
2026-04-03 19:00:01 -0400

    Fix middleware class name in settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -73,7 +73,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "mysite.middleware.EnforcePasswordChangeMiddleware",
+    "mysite.middleware.PtoolAccessMiddleware",
 ]
 
 ROOT_URLCONF = "mysite.urls"
