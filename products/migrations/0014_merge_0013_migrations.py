021508a53d6040f6940815bfeec64703bd4ef677
Alex <alexkharrod@gmail.com>
2026-04-04 10:09:24 -0400

    Add merge migration for conflicting 0013s

---
+++ products/migrations/0014_merge_0013_migrations.py
@@ -0,0 +1,12 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('products', '0013_alter_htscode_category_hint_and_more'),
+        ('products', '0013_htscode_extra_tariff'),
+    ]
+
+    operations = [
+    ]
