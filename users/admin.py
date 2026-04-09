0c885d21761d5d3b69d3d59435cc7d045b30de4c
Alex <alexkharrod@gmail.com>
2026-04-09 08:13:08 -0400

    Fix Django admin 500: add readonly_fields for date_joined, add access flag fieldsets

--- users/admin.py
+++ users/admin.py
@@ -6,19 +6,25 @@ from .models import CustomUser
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
-    list_display = ('email', 'first_name', 'last_name', 'is_staff', 'role')
+    list_display = ('email', 'first_name', 'last_name', 'is_staff',
+                    'access_products', 'access_quotes', 'access_scouting')
+    readonly_fields = ('last_login', 'date_joined')
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
         ('Personal Info', {'fields': ('first_name', 'last_name')}),
-        ('Permissions', {'fields': ('is_staff', 'is_superuser', 'is_active', 'groups', 'user_permissions')}),
+        ('Section Access', {'fields': ('access_products', 'access_quotes', 'access_scouting')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
         ('Important Dates', {'fields': ('last_login', 'date_joined')}),
-        ('Additional Info', {'fields': ('role',)}),
+        ('Legacy', {'fields': ('role', 'scouting_only', 'must_change_password'),
+                    'classes': ('collapse',)}),
     )
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2', 'first_name', 'last_name', 'is_staff', 'is_active', 'role')}
-        ),
+            'fields': ('email', 'password1', 'password2', 'first_name', 'last_name',
+                       'access_products', 'access_quotes', 'access_scouting',
+                       'is_staff', 'is_active'),
+        }),
     )
     search_fields = ('email', 'first_name', 'last_name')
     ordering = ('email',)
